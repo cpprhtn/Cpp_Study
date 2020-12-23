b0138c748bda8494ff6136a14bf433ef1549d83f
Lee_JunWon <cpprhtn@naver.com>
2020-12-23 17:05:54 +0900

    Update Game_Information.cpp

--- 011/Game_Information.cpp
+++ 011/Game_Information.cpp
@@ -1,5 +1,6 @@
 #include "Game_Information.h"
 
+// 기본 생성자
 info::info(){   // 기본 생성자 정의
     age = 0;
     ID = "";
@@ -20,4 +21,26 @@ void info::set(string name, int n){
 
 void info::print(){
     cout << "아이디: " << ID << ", 나이: " << age << endl;
+}
+
+
+// 생성자 오버로딩
+Users::Users():name("비공개"),years(2020){
+
+}
+
+Users::Users(string names):name(names),years(2020){
+
+}
+
+Users::Users(int n):name("비공개"),years(n){
+
+}
+
+Users::Users(string names, int n):name(names),years(n){
+
+}
+
+void Users::print(){
+    cout << "사용자명: " << name << ", 생성년도: " << years << endl;
 }

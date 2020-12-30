bbfec7689190224f47b438d806755759a6c5cce2
Lee_JunWon <cpprhtn@naver.com>
2020-12-30 16:55:36 +0900

    Create main2.cpp

---
+++ 012/main2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Location.h"
+using namespace std;
+
+int main(){
+
+    cout << "9강에서 사용한 Location class" << endl;
+    Location Two_dot;
+    Two_dot.setQ(-1, 2);
+    Two_dot.setR(-5, -5);
+    Two_dot.print();
+
+    cout << "복사 생성자를 통한 복사" << endl;
+    Location copy_dot(Two_dot);
+    copy_dot.print();
+
+    return 0;
+}
